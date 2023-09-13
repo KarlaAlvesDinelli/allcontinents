6b0bf7ae53ea47486fc34142bbdc073488a03358
KarlaAlvesDinelli <karlaalvesjobsdev@gmail.com>
2023-09-13 12:45:12 -0300

    all continents

---
+++ index.tsx
@@ -0,0 +1,54 @@
+function allContinents(list) {
+  const continents = Array.from(
+    new Set(list.map((developer) => developer.continent))
+  );
+  return ["Africa", "Americas", "Asia", "Europe", "Oceania"].every(
+    (continent) => continents.includes(continent)
+  );
+}
+
+const developerList = [
+  {
+    firstName: "Fatima",
+    lastName: "A.",
+    country: "Algeria",
+    continent: "Africa",
+    age: 25,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Agustín",
+    lastName: "M.",
+    country: "Chile",
+    continent: "Americas",
+    age: 37,
+    language: "C",
+  },
+  {
+    firstName: "Jing",
+    lastName: "X.",
+    country: "China",
+    continent: "Asia",
+    age: 39,
+    language: "Ruby",
+  },
+  {
+    firstName: "Laia",
+    lastName: "P.",
+    country: "Andorra",
+    continent: "Europe",
+    age: 55,
+    language: "Ruby",
+  },
+  {
+    firstName: "Oliver",
+    lastName: "Q.",
+    country: "Australia",
+    continent: "Oceania",
+    age: 65,
+    language: "PHP",
+  },
+];
+
+const result = allContinents(developerList);
+console.log(result);
